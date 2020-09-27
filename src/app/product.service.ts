2dce6bc14919b1fc00e6a7895d5c1f11bceacff2
Michael August <michaelfrancis135@gmail.com>
2020-09-27 23:49:56 +0100

    interface implementation

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
 import { Http, Response } from '@angular/http'
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
@@ -10,8 +12,8 @@ export class ProductService {
 
   private _albumUrl = "../assets/album.json"
 
-  getAlbum(id: number){
+  getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl)
-    .map(response => response.json())
+    .map((response) => response.json() as Album)
   }
 }
